00ce5b93bb234efa9a78e5970413abd90e7d4de6
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-04 12:13:13 -0400

    erasing comments

--- filtrando.rb
+++ filtrando.rb
@@ -1,12 +1,3 @@
-# Dado un hash crear un método que devuelva otro hash pero filtrando todos aquellos que tienen
-# valores inferior a 70000
-# Probar con el siguiente hash
-
-# ventas = {
-# Octubre: 65000,
-# Noviembre: 68000,
-# Diciembre: 72000
-# }
 
 def filter( param )
     new__hash = {}
